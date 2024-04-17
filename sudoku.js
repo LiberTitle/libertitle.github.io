a4261830f607ec86ef437a81f195e9917e5ffa1b
CallMePierson <alisonepierson@outlook.com>
2024-04-17 12:50:40 -0400

    was worried to publish, but need to get a move on

---
+++ sudoku.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    setGame();
+}
+function setGame() {
+    let game = [0,0,0,0,0,0,0,9,0,
+                0,3,0,0,0,0,0,2,8,
+                6,0,0,8,0,0,5,7,0,
+                0,1,9,0,2,6,0,8,0,
+                0,0,4,0,0,9,0,0,0,
+                7,0,0,4,0,0,0,0,0,
+                8,9,7,3,0,4,6,0,0,
+                3,2,6,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0];
+    game = game.reverse();
+    document.querySelectorAll("div.box input").forEach(box => {
+        
+        let square = game.pop();
+        if (square != 0) {
+            box.value = square;
+            box.disabled = "true";
+        }
+    });
+}
